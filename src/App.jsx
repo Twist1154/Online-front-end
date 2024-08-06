5e922129a89f4448957ad319f2fd2f86df99f832
Twist1154n <rethabile1154n@gmail.com>
2024-08-06 20:23:29 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import PrimarySearchAppBar from './components/PrimarySearchAppBar'; // Adjust the path if needed
+
+function App() {
+  return (
+    <div className="main" style={{ width: "100vw", justifyContent: "center" }}>
+      <PrimarySearchAppBar />
+      <div style={{ padding: '20px' }}>
+        {/* Your other components and content go here */}
+        <h1 >Welcome to Our Capstone landing page for Admins</h1>
+        <p>Rethabile Ntsekhe 220455430</p>
+      </div>
+    </div>
+  );
+}
+export default App;
